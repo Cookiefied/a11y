0372fd9a6f6d08b1cd99926b10255355e1e52b37
Ricky Gibbons <105470662+Cookiefied@users.noreply.github.com>
2023-09-21 11:19:26 -0500

    Build basic website, icons made and logos

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './images/a11y_square.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This site is a work in progress. Please check back later.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
