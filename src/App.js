bf2958d7cf1cd8b0360405440be31a8371828b8c
Ricky Gibbons <105470662+Cookiefied@users.noreply.github.com>
2023-10-05 13:32:44 -0500

    Added files for basic website

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from './images/a11y_square.png';
+import React from 'react';
+import logo from './images/a11y_circle.png';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Project from "./components/pages/Project";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This site is a work in progress. Please check back later.
-        </p>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Project />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
