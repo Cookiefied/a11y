ac5253b54807c09a1b8248f773e90960d402b4bb
atayyari <39579034+atayyari@users.noreply.github.com>
2023-10-05 19:27:11 -0500

    Catalog page content

--- src/App.js
+++ src/App.js
@@ -10,6 +10,19 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
+        <h1>Designing Accessible Websites</h1>
+        <hr>
+        </hr>
+
+        <h2>Principle 1: Perceivability</h2>
+        <p>
+        A blurb about perceivability and its importance. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        </p>
+
+        <h2>Principle 2: Operability</h2>
+        <p>
+        A blurb about operability and its importance. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        </p>
         <Routes>
           <Route path="/" element={<Project />}></Route>
         </Routes>
