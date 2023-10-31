07c65d0a8b1b524a0456a7ffe2a498ee0d198def
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2023-10-31 12:01:09 -0500

    Update Sidebar.js
    add background accurate to site

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -296,7 +296,7 @@ function Sidebar() {
                                 ref={descriptionElementRef}
                                 tabIndex={-1}
                             >
-                                <p>The goal of this project is to provide a solution to an issue with media such as posters, images, and graphics being inaccessible to colour blind people. A team member’s parent and friend are color blind and we were inspired by the demonstrated project on poster accessibility. We decided to improve upon it, providing a quick solution to checking the color blind accessibility to graphics, posters, and other media. </p>
+                                <p>The A11y Upgrades project is aimed at creating and maintaining an information-based website. The project's primary objectives are to promote accessibility awareness, and test the knowledge gained by interacting with our website.</p>
                                 <p>This is our implementation of a web application where all can upload an image and our application will scan it and report how accessible the image is for different types of color-blindess, based on user testing. Our application will give the image a rating for Monochromacy and the three variations of Dichromacy. It will also explain how to improve the ratings for each, and link to background information on colour blindness. </p>
                             </DialogContentText>
                             </DialogContent>
